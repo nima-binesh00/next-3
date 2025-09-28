424c307e59cb0cee2af4cfde47a54a2a1daa3bfc
nima-binesh00 <nimabinesh00@gmail.com>
2025-09-28 19:45:43 +0330

    add

--- app/page.jsx
+++ app/page.jsx
@@ -1,42 +1,49 @@
 "use client";
 import { Typography } from "@mui/material";
 import { Box, padding } from "@mui/system";
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
   return (
     <div>
       <Box sx={{ padding: "0 20px" }}>
-        <Box
-          fullWidth
-          justifyContent={"center"}
-          marginTop={"50px"}
-          sx={{ border: "1px solid grey", padding: "40px 0" }}
-        >
-          <Typography variant="h1" textAlign={"center"}>
-            Users
-          </Typography>
-        </Box>
-        <Box
-          fullWidth
-          justifyContent={"center"}
-          marginTop={"40px"}
-          sx={{ border: "1px solid grey", padding: "40px 0" }}
-        >
-          <Typography variant="h1" textAlign={"center"}>
-            posts
-          </Typography>
-        </Box>
-        <Box
-          fullWidth
-          justifyContent={"center"}
-          marginTop={"40px"}
-          sx={{ border: "1px solid grey", padding: "40px 0" }}
-        >
-          <Typography variant="h1" textAlign={"center"}>
-            recipes
-          </Typography>
-        </Box>
+        <Link href={"/users"}>
+          <Box
+            fullWidth
+            justifyContent={"center"}
+            marginTop={"50px"}
+            sx={{ border: "1px solid grey", padding: "40px 0" }}
+          >
+            <Typography variant="h1" textAlign={"center"}>
+              Users
+            </Typography>
+          </Box>
+        </Link>
+        <Link href={"/posts"}>
+          <Box
+            fullWidth
+            justifyContent={"center"}
+            marginTop={"40px"}
+            sx={{ border: "1px solid grey", padding: "40px 0" }}
+          >
+            <Typography variant="h1" textAlign={"center"}>
+              posts
+            </Typography>
+          </Box>
+        </Link>
+        <Link href={"/recipes"}>
+          <Box
+            fullWidth
+            justifyContent={"center"}
+            marginTop={"40px"}
+            sx={{ border: "1px solid grey", padding: "40px 0" }}
+          >
+            <Typography variant="h1" textAlign={"center"}>
+              recipes
+            </Typography>
+          </Box>
+        </Link>
       </Box>
     </div>
   );
